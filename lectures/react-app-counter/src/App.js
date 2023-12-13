2dc11ba2be7c814e0452b2adcb4247f2e5da913f
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-12-13 07:33:48 -0500

    Refactor counting in react

--- lectures/react-app-counter/src/App.js
+++ lectures/react-app-counter/src/App.js
@@ -1,77 +1,69 @@
 import React from "react";
 import "./App.css";
 
-class Button extends React.Component {
-    render() {
-        return (
-            <button onClick={this.props.onClick}>{this.props.children}</button>
-        );
-    }
-}
-
-// class App extends React.Component {
-//     // counter = 0;
-//     constructor() {
-//         super();
-//         this.state = {
-//             counter: 0
-//         };
-//     }
-
-//     handleOnClick = (e) => {
-//         this.setState({ counter: this.state.counter + 1 });
-//     };
-//     render() {
-//         return (
-//             <div>
-//                 <Button onClick={this.handleOnClick}>Click Me</Button>
-//                 <span>Counter: {this.state.counter}</span>
-//             </div>
-//         );
-//     }
-// }
-
 class App extends React.Component {
-    state = {
-        showMenu: false
+    constructor() {
+        super();
+        this.state = {
+            counter: 0
+        };
+    }
+    increment = () => {
+        this.setState((prevState) => ({
+            counter: prevState.counter + 1
+        }));
     };
-
-    openMenu = () => {
-        this.setState({
-            showMenu: true
-        });
-    };
-    closeMenu = () => {
-        this.setState({
-            showMenu: false
-        });
+    decrement = () => {
+        this.setState((prevState) => ({
+            counter: prevState.counter - 1
+        }));
     };
     render() {
         return (
             <main>
-                <button onClick={this.openMenu}>Menu</button>
-                {this.state.showMenu ? (
-                    <div className="menu">
-                        <button onClick={this.closeMenu}>X</button>
-                    </div>
-                ) : null}
+                <button onClick={this.decrement}>-</button>
+                <span>Counter:{this.state.counter}</span>
+                <button onClick={this.increment}>+</button>
             </main>
         );
     }
 }
 
-// class App extends React.Component {
-//     handleOnClick = () => {
-//         console.log("Hello");
-//     };
+// class Button extends React.Component {
 //     render() {
 //         return (
-//             <div>
-//                 <Button>Click Me</Button>
-//                 {/* <Button1>Click Me1</Button1> */}
-//                 <span>Counter: 0</span>
-//             </div>
+//             <button onClick={this.props.onClick}>{this.props.children}</button>
 //         );
 //     }
 // }
+
+// class App extends React.Component {
+//     state = {
+//         showMenu: false
+//     };
+
+//     openMenu = () => {
+//         this.setState({
+//             showMenu: true
+//         });
+//     };
+//     closeMenu = () => {
+//         this.setState({
+//             showMenu: false
+//         });
+//     };
+//     render() {
+//         return (
+//             <main>
+//                 <button onClick={this.openMenu}>Menu</button>
+//                 {this.state.showMenu ? (
+//                     <div className="menu">
+//                         <button onClick={this.closeMenu}>X</button>
+//                     </div>
+//                 ) : null}
+//             </main>
+//         );
+//     }
+// }
+
 export default App;
